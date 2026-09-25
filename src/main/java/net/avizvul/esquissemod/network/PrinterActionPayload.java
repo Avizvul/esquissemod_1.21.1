50c2de24ebad0bbf73f275b4ce7ed8a2a30dc637
Avizvul <mineriskur1@gmail.com>
2026-09-25 14:34:20 +0300

    update

--- src/main/java/net/avizvul/esquissemod/network/PrinterActionPayload.java
+++ src/main/java/net/avizvul/esquissemod/network/PrinterActionPayload.java
@@ -8,5 +8,11 @@ import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
 
 public record PrinterActionPayload(BlockPos pos) implements CustomPacketPayload {
-    public static final Type
+    public static final Type<PrinterActionPayload> TYPE = new Type<>(ResourceLocation.fromNamespaceAndPath(EsquisseMod.MOD_ID, "printer_action"));
+    public static final StreamCodec<RegistryFriendlyByteBuf, PrinterActionPayload> STREAM_CODEC = StreamCodec.composite(BlockPos.STREAM_CODEC, PrinterActionPayload::pos, PrinterActionPayload::new);
+
+    @Override
+    public Type<? extends CustomPacketPayload> type() {
+        return TYPE;
+    }
 }

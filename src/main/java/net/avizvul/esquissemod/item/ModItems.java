f42f093ae318b605fd7a10accaa5ac18c441db77
Avizvul <mineriskur1@gmail.com>
2026-09-25 16:59:42 +0300

    update

--- src/main/java/net/avizvul/esquissemod/item/ModItems.java
+++ src/main/java/net/avizvul/esquissemod/item/ModItems.java
@@ -8,6 +8,7 @@ import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
+import net.avizvul.esquissemod.block.ModBlocks;
 
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(EsquisseMod.MOD_ID);
